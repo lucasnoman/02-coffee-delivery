256cf5b250a708c2e00ee5337dff9dc9077c549d
Lucas C. Noman <lucas.noman@outlook.com>
2024-08-05 20:32:50 -0300

    build: configures eslint for tailwind, tanstack router and some presets for tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
     '@rocketseat/eslint-config/react',
+    "plugin:tailwindcss/recommended"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,35 @@ module.exports = {
       },
     },
     extend: {
+      colors: {
+        'yellow-dark': '#C47F17',
+        'yellow-default': '#DBAC2C',
+        'yellow-light': '#F1E9C9',
+        'purple-dark': '#4B2995',
+        'purple-default': '#8047F8',
+        'purple-light': '#EBE5F9',
+        'base-title': '#272221',
+        'base-subtitle': '#403937',
+        'base-text': '#574F4D',
+        'base-label': '#8D8686',
+        'base-hover': '#D7D5D5',
+        'base-button': '#E6E5E5',
+        'base-input': '#EDEDED',
+        'base-card': '#F3F2F2',
+        'base-background': '#FAFAFA',
+      },
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+        baloo: '"Baloo 2", cursive',
+      },
+      fontSize: {
+        base: ['1rem', '1.3'],
+        xl: ['1.25rem', '1.3'],
+        '5xl': ['3rem', '1.3'],
+      },
+      backgroundImage: {
+        intro: "url('/src/assets/intro-background.png')",
+      },
       keyframes: {
         'accordion-down': {
           from: { height: '0' },
